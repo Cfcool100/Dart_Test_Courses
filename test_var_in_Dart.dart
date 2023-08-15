67730e9a403faae41e45e52b1228df44d16585ac
Cfcool100 ~ % git config --global user.name Youssouf <cfcool100@gmail.com>
2023-08-15 20:06:05 +0000

    first test dart (datatype and variable)

---
+++ test_var_in_Dart.dart
@@ -0,0 +1,38 @@
+main(){
+
+
+
+
+
+
+
+  // id card
+  var lastName = "SISSOKO";
+  var firstName = "Youssouf";
+  double size = 1.67;
+  int age = 25;
+  String profession = "FullStack Developer";
+  var birthDay = DateTime(2003, 3, 1);
+  var delivery = DateTime(2023, 6, 12);
+  Final expired = DateTime(2027, 6, 12);
+
+  print('''
+  LastName: $lastName
+
+  FirstName: $firstName
+
+  Birthday: $birthDay
+
+  Age: $age
+
+  Size: $size
+
+  Profession: $profession
+
+
+  
+                                              Delivery: $delivery
+
+                                                  Expired: $expired
+  ''');
+}
