61e0da8b92c8eedb363d9e143a0b51d37d9b5f90
Cfcool100 ~ % git config --global user.name Youssouf <cfcool100@gmail.com>
2023-08-16 14:06:52 +0000

    if else / control flow statement

---
+++ control-flow-statement.dart
@@ -0,0 +1,80 @@
+// if else statement
+/*
+import 'dart:io';
+
+void main() {   
+  print("Enter the month number betwen 1-12:");
+  var input = stdin.readLineSync();
+
+  if (input != null) {
+    int months = int.parse(input);
+    
+    if (months <= 12) {
+      print('Hello, we are in ${months}e  month of year ');
+    }
+  } else {
+    print("Invalid input");
+  }
+ } 
+*/
+
+
+// if elseIf else statement
+/*
+import 'dart:io';
+
+void main() {   
+  print("Enter the month number betwen 1-12:");
+  var input = stdin.readLineSync();
+
+  if (input != null) {
+    int months = int.parse(input);
+    
+    if (months <= 12) {
+      print('Hello, we are in ${months}e  month of year ');
+    }else if ( months > 12) {
+    print("ok this month not exist 😀");
+    }
+  } else {
+    print("Invalid input");
+  }
+} 
+*/
+
+// if statement with break keyword
+
+/*
+ void main() {   
+  for(int i=1;i<=10;i++){    
+    if(i==5){    
+      print("Hello");  
+      break;         
+    }    
+    print(i);    
+  }   
+}
+*/
+
+// if statement with continue keyword
+/*
+ void main() {   
+  for(int i=1;i<=10;i++){    
+    if(i==5){    
+      print("Hello");  
+      continue; 
+    }    
+    print(i);    
+  }   
+}
+*/
+
+
+
+
+// void main() {   
+//    var names = ["Peter", "Rinky Ponting", "Abhishek"];   
+     
+//    for (var name in names) {   
+//       print(name);   
+//    }   
+// }  
