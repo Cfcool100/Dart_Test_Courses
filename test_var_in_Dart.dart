c9769333f1fb03389becaffb190cba6e703a098e
Cfcool100 ~ % git config --global user.name Youssouf <cfcool100@gmail.com>
2023-08-16 11:48:35 +0000

    fix 'final' constante

--- test_var_in_Dart.dart
+++ test_var_in_Dart.dart
@@ -9,19 +9,22 @@ main(){
   // id card
   const lastName = "SISSOKO";
   const firstName = "Youssouf";
+  const gender = "Male";
   double size = 1.67;
   int age = 25;
   String profession = "FullStack Developer";
   var birthDay = DateTime(2003, 3, 1);
   var delivery = DateTime(2023, 6, 12);
-  Final expired = DateTime(2027, 6, 12);
+  final expired = DateTime(2027, 6, 12);
 
   print('''
   LastName: $lastName
 
   FirstName: $firstName
 
-  Birthday: $birthDay
+  Birthday: $birthDay 
+
+  gender: $gender (${gender == "Male"})
 
   Age: $age
 
